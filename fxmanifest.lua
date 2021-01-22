87e9f366ad71876692751badf49a195e5ff71fa1
YourEyes <72436392+YourEyesGG@users.noreply.github.com>
2021-01-22 07:39:54 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'YourEyes'
+description 'NPC Medis'
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'client.lua',
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'server.lua',
+}
